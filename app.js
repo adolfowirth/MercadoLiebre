592d67bcf2215004c3c02107a54cc7a82a149618
unknown <you@example.com>
2022-05-08 22:48:04 -0300

    finalizando mercadoliebre

--- app.js
+++ app.js
@@ -14,3 +14,12 @@ app.get('/', (req,res) => {
     res.sendFile(htmlPath);
 })
 
+app.get('/register', (req,res) => {
+    let htmlPath = path.resolve(__dirname, './views/register.html');
+    res.sendFile(htmlPath);
+})
+
+app.get('/login', (req,res) => {
+    let htmlPath = path.resolve(__dirname, './views/login.html');
+    res.sendFile(htmlPath);
+})
